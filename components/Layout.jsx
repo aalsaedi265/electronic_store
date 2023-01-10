348ff041406239708603922fecdcc7710ebb04d2
aalsaedi265 <aalsaedi265@gmail.com>
2023-01-10 06:01:17 -0600

    layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Head from 'next/head'
+import Navbar from './Navbar'
+import Footer from './Footer'
 
-const layout = () => {
+const layout = ({children}) => {
   return (
-    <div>layout</div>
+    <div className='layout'>
+      <Head>
+        <title>Stud Electronics</title>
+      </Head>
+      <header>
+        <Navbar/>
+      </header>
+      <main className='main'>
+        {children}
+      </main>
+      <footer>
+        <Footer/>
+      </footer>
+      
+    </div>
   )
 }
 
